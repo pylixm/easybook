3bae39c989babdb73ff0c80cc9497ebf398920c6
Liu Feng <s_t_s_l_f@163.com>
2017-09-12 19:22:09 +0800

    增加详细注释和优化代码格式

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="wrap">
-      <top-nav/>
-      <back-to-top/>
-      <transition name="fade">
-          <keep-alive include="index">
-              <router-view></router-view>
-          </keep-alive>
-      </transition>
-      <!-- 下面div绑定@click="changeColor" -->
-      <div class="footer" > 
-          <router-link to="/list" @click.native="changeColor('list')" class="tab" :class="[ activeTab === 'list' ?  'active' : '' ]"><i class="czs-home-l"></i>首页</router-link>      
-          <router-link to="/me" @click.native="changeColor('me')" class="tab" :class="[ activeTab === 'me' ?  'active' : '' ]"><i class="czs-user-l"></i>我</router-link>
-      </div>
-  </div>
+    <div class="wrap">
+        <top-nav/>
+        <back-to-top/>
+        <transition name="fade">
+            <keep-alive include="index">
+                <router-view></router-view>
+            </keep-alive>
+        </transition>
+        <!-- 下面div绑定@click="changeColor" -->
+        <div class="footer" > 
+            <router-link to="/list" @click.native="changeColor('list')" class="tab" :class="[ activeTab === 'list' ?  'active' : '' ]"><i class="czs-home-l"></i>首页</router-link>      
+            <router-link to="/me" @click.native="changeColor('me')" class="tab" :class="[ activeTab === 'me' ?  'active' : '' ]"><i class="czs-user-l"></i>我</router-link>
+        </div>
+    </div>
 </template>
 <script>
 import topNav from '@/components/topNav.vue';
@@ -45,19 +45,22 @@ export default {
         //         }
         //     }
         // },
+        
+
+        // 点击后设置store中activeTab的值，然后切换标签通过判断activeTab值是否是本省来设置活动状态
         changeColor(tab) {
             switch(tab) {
                 case 'list':
                     this.$store.commit(types.SET_ACTIVETAB,'list');
-                    console.log(this.activeTab);
+                    // console.log(this.activeTab);
                     break;
                 case 'me':
                     this.$store.commit(types.SET_ACTIVETAB, 'me');
-                    console.log(this.activeTab);
+                    // console.log(this.activeTab);
                     break;
                 default: 
                     this.$store.commit(types.SET_ACTIVETAB, 'list');
-                    console.log(this.activeTab);
+                    // console.log(this.activeTab);
                     break;
             }
         },
@@ -74,7 +77,7 @@ export default {
             position: fixed;
             bottom: 0;
             left: 0;
-            width: 100vw;
+            width: 100%;
             display: flex;
             border-top: 1px solid $separate-line-color; 
             background-color: $color1;
@@ -99,19 +102,19 @@ export default {
     }
 
     .fade-enter-active {
-    animation: zoomIn .5s;
+        animation: zoomIn .5s;
     }
     .fade-leave-active {
-    animation: none;
+        animation: none;
     }
     @keyframes zoomIn {
-    from {
-        opacity: 0;
-        transform: scale3d(.3, .3, .3)
-    }
-    50% {
-        opacity: 1;
-    }
+        from {
+            opacity: 0;
+            transform: scale3d(.3, .3, .3)
+        }
+        50% {
+            opacity: 1;
+        }
     }
 </style>
 
